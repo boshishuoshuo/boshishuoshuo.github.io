19f2ba666afd84ba3f73287bfb1043789802b5f4
Yan Feng <fengyan0121@gmail.com>
2024-03-12 16:55:17 -0400

    Update diplomatic_sum.py to generate content for video

--- script/diplomatic_sum.py
+++ script/diplomatic_sum.py
@@ -2,6 +2,22 @@
 
 import pandas as pd
 import sys
+import calendar
+
+def get_action_word(word, number, sentence = False):
+    if word == 'phone':
+        word += " call"
+    if sentence:
+        if 'Beijing' in word:
+            word = word.replace('visit', 'guest')
+        elif 'abroad' in word:
+            word = 'country'
+    if number == 1:
+        return word
+    if 'visit' in word or 'guest' in word or 'country' in word:
+        return word.replace('visit', 'visits').replace('guest', 'guests').replace('country', 'countries')
+    else:
+        return word + 's'
 
 diplomatic_activities = '../_data/diplomatic_activities.csv'
 
@@ -13,12 +29,32 @@ year = int(sys.argv[2])
 df_filtered = dip_ac_df[(dip_ac_df['date'].dt.month == month) \
                         & (dip_ac_df['date'].dt.year == year) ]
 
-df_filtered = df_filtered.sort_values(by='format')
+df_filtered = df_filtered.sort_values(by=['format', 'date'])
 
 df_filtered['Count'] = df_filtered.groupby('format').cumcount() + 1
 
 df_count = df_filtered.pop('Count')
 
-df_filtered.insert(2,'Count', df_count)
+df_filtered.insert(0,'Count', df_count)
 
-df_filtered.to_csv('../_data/diplomatic_{}_{}.csv'.format(year,month), index=False, date_format='%m/%d/%Y')
+df_filtered.to_csv('../_data/diplomatic_{}_{}.csv'.format(year,month), index=False, date_format='%Y %b %d')
+
+format_dict = df_filtered['format'].value_counts().to_dict()
+verb_dict = {'letter': 'replied',
+             'message': 'sent',
+             'phone': 'made',
+             'video chat': 'made',
+             'visit in Beijing': 'met',
+             'visit abroad': 'visited'}
+
+with open(f'../video_materials/content_{year}_{month}.txt', 'w') as cf:
+    cf.write(f'Your diplomatic activities in {calendar.month_name[month]} of {year} includes ')
+    for k, v in format_dict.items():
+        cf.write(f'{v} {get_action_word(k, v)} ')
+    cf.write('.\n')
+    for k, v in format_dict.items():
+        cf.write(f'\nYou {verb_dict[k]} {v} {get_action_word(k,v, True)}:\n')
+        df_filtered_format = df_filtered[df_filtered['format'] == k]
+        for index, row in df_filtered_format.iterrows():
+            cf.write(f'{row["Count"]} On {row["date"].strftime("%Y %B %d")}, To {row["country"]} {row["counterparty"]} for {row["message"]} \n')
+        
